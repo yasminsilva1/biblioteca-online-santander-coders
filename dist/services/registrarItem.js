9108789d2ee08eac206fefed3bc8f48686b8571d
Gabryel Costa <114967527+gabryelcosta@users.noreply.github.com>
2024-09-30 12:31:12 -0300

    Delete dist directory

--- dist/services/registrarItem.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.registrarItem = registrarItem;
-function registrarItem(biblioteca, item) {
-    biblioteca.adicionarItem(item);
-}
