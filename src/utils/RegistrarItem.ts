444d9b81bff9f1faed70400f49643cb24f33cd2d
Gabryel Costa <114967527+gabryelcosta@users.noreply.github.com>
2024-09-30 12:46:41 -0300

    Delete src/utils/RegistrarItem.ts

--- src/utils/RegistrarItem.ts
+++
@@ -1,8 +0,0 @@
-import ItemAcervo from '../models/ItemAcervo';
-import Biblioteca from '../models/Biblioteca';
-
-function RegistrarItem(biblioteca: Biblioteca, item: ItemAcervo) {
-    biblioteca.adicionarItem(item);
-}
-
-export default RegistrarItem;
