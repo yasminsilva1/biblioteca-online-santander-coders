75ae9a8d17ddefddc254b80f0b6c51d04b6d6464
Gabryel Costa <114967527+gabryelcosta@users.noreply.github.com>
2024-09-30 12:28:18 -0300

    Delete src/services directory

--- src/services/registrarItem.ts
+++
@@ -1,6 +0,0 @@
-import { ItemAcervo } from '../models/ItemAcervo';
-import { Biblioteca } from './Biblioteca';
-
-export function registrarItem<T extends ItemAcervo>(biblioteca: Biblioteca, item: T): void {
-    biblioteca.adicionarItem(item);
-}
